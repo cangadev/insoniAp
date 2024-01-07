795592f3fa80b9bdd6564686291d75b30687255c
CangaDev <cangadev7@gmail.com>
2024-01-07 18:14:23 +0100

    fix build bug

---
+++ build/precache-manifest.0083ec74347420d34c928613ee0b4632.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7666db30f78d10a911db6099627e0b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e429f0ba81df68c2b995",
+    "url": "/static/css/main.1d6c45ec.chunk.css"
+  },
+  {
+    "revision": "51439635ca159792577c",
+    "url": "/static/js/2.d9985715.chunk.js"
+  },
+  {
+    "revision": "98ba7388b8e6f4cfa8d1",
+    "url": "/static/js/3.34b8d668.chunk.js"
+  },
+  {
+    "revision": "e429f0ba81df68c2b995",
+    "url": "/static/js/main.32540020.chunk.js"
+  },
+  {
+    "revision": "d9f55312c8b8c072fe13",
+    "url": "/static/js/runtime~main.5b227698.js"
+  },
+  {
+    "revision": "5860519b5150e11c36d591adfe829abc",
+    "url": "/static/media/079fb574-e319-493d-a49f-e8fed3994201.5860519b.png"
+  },
+  {
+    "revision": "aac7b5d081ddb514d2de40313154d040",
+    "url": "/static/media/add.aac7b5d0.svg"
+  },
+  {
+    "revision": "8155183370a9abeaac19596b6da957f3",
+    "url": "/static/media/carrinho.81551833.svg"
+  },
+  {
+    "revision": "eee641ebc5234c053e8ac7552473d3b7",
+    "url": "/static/media/categoria.eee641eb.svg"
+  },
+  {
+    "revision": "fd764cabebef04fd4ee01e6c199c2b38",
+    "url": "/static/media/conta.fd764cab.svg"
+  },
+  {
+    "revision": "988a3f96552a7dbe944f194f6e6fcc63",
+    "url": "/static/media/favorite-border.988a3f96.svg"
+  },
+  {
+    "revision": "599b63f67df2fb6901ae113772069619",
+    "url": "/static/media/free.599b63f6.svg"
+  },
+  {
+    "revision": "9a6ca716249223bdc1e89dbf1792a665",
+    "url": "/static/media/high-quality.9a6ca716.svg"
+  },
+  {
+    "revision": "4f59fafb4cd475d09d2170f915adc16e",
+    "url": "/static/media/logout.4f59fafb.svg"
+  },
+  {
+    "revision": "9009a79a54c3c5cdb3b001224393d5d6",
+    "url": "/static/media/lupa.9009a79a.svg"
+  },
+  {
+    "revision": "1161fcc0c7a39666948ed2739de5fd21",
+    "url": "/static/media/no-photo.1161fcc0.svg"
+  },
+  {
+    "revision": "bee9a1b878735944a1061932bda12606",
+    "url": "/static/media/remover.bee9a1b8.svg"
+  },
+  {
+    "revision": "3044b173fbed65de8fd654a60ab52da7",
+    "url": "/static/media/saving-img.3044b173.png"
+  },
+  {
+    "revision": "00fcf9b5806e3d4c18138b17e3a4b045",
+    "url": "/static/media/seta-baixo-12.00fcf9b5.svg"
+  },
+  {
+    "revision": "171b9cef295ac5820932f587cbfa7b4e",
+    "url": "/static/media/seta-direita.171b9cef.svg"
+  },
+  {
+    "revision": "542c1d654c1a9c61305a7e13291a2b4c",
+    "url": "/static/media/single-image3.542c1d65.jpg"
+  },
+  {
+    "revision": "78cc1c13aa00c72e8d33d58bd5640bb9",
+    "url": "/static/media/suporte.78cc1c13.svg"
+  },
+  {
+    "revision": "b279be987e5eab92b76961f2fce0360a",
+    "url": "/static/media/truck.b279be98.svg"
+  }
+]);
